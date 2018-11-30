88a80405a8edfba00786121508ebd7f782a3ea72
Thavriliak <Havriliakt@gmail.com>
2018-11-30 09:10:36 -0500

    Fixed trailing backslash

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://morning-gorge-80977.herokuapp.com/',
+  production: 'https://morning-gorge-80977.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
