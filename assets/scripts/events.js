86ce78fdfa9d940f9b821fc4c1e19be923968985
Thavriliak <Havriliakt@gmail.com>
2019-01-22 11:40:28 -0500

    Started working with auth CRUD actions

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,8 +1,6 @@
-const store = require('./store.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 const getFormFields = require('../../lib/get-form-fields.js')
-const config = require('./config.js')
 
 const onSignUp = event => {
   event.preventDefault()
